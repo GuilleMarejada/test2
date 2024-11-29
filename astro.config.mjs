27d23f65718986a93e4a806c818523e54d20e546
GuilleMarejada <guillermo.gil.cor@gmail.com>
2024-11-29 11:23:29 +0000

    eliminamos serverless de astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import vercel from '@astrojs/vercel/serverless';
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel()
+  output: "server",
+  adapter: vercel(),
 });
